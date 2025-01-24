e741b317edd60aff20aa21f66f94cde55d72647f
Simonas <Simonas.stanislovaitis@gmail.com>
2025-01-24 02:12:19 +0200

    Add input field to display text in blue

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Welcome to My Modern Webpage");
+
+function displayText() {
+    const input = document.getElementById("textInput").value;
+    const output = document.getElementById("outputText");
+
+    output.textContent = input;
+    output.style.color = "blue";
+}
